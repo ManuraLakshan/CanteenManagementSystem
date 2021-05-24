b58f75fd7a8bb465df799d776f6ed832bfdfb3e3
Kelum nagodavithana <54311397+sanjuTec@users.noreply.github.com>
2021-05-24 20:55:06 +0530

    Canteen Owner Entity
    Co-Authored-By: M.Sujan <73443739+Sujan-Murugesh@users.noreply.github.com>

---
+++ src/main/java/com/canteen/Fot/CanteenOwner.java
@@ -0,0 +1,39 @@
+package com.canteen.Fot;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class CanteenOwner {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer owner_id;
+    private String name;
+    private Integer phone;
+
+    public Integer getOwner_id() {
+        return owner_id;
+    }
+
+    public void setOwner_id(Integer owner_id) {
+        this.owner_id = owner_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Integer phone) {
+        this.phone = phone;
+    }
+}
